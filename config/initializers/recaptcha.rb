82c8d7c0f320848c31634c28c9574c295ae258d6
AndreiZykov <zykovwat2008@gmail.com>
2016-12-18 02:31:23 -0500

    recapcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-config.site_key = '6LcKuwwUAAAAAAWBB0JH5MDquWpuBaxuqud71C91' 
-config.secret_key = '6LcKuwwUAAAAAFBdsd4YNEkGiDeuJ9qBI6juUEe_' 
+config.site_key = '6LdoFw8UAAAAAFr-16_Ydc9JjYpPEDwlmAbeEUxX' 
+config.secret_key = '6LdoFw8UAAAAAPNLLeEMW0qyzGE_SMzsao-7O1IG' 
 end
